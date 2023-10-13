d8b2c68ba8fbb882d8905cb98c7212ccbd7b304a
chuvietanh392003 <130541979+chuvietanh392003@users.noreply.github.com>
2023-10-13 23:11:38 +0700

    new prj

---
+++ src/week1/Bai1.java
@@ -0,0 +1,8 @@
+package week1;
+//Example 1: HelloWorld.ja
+public class Bai1{
+	public static void main(String[] args) {
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+	} //end of method main
+}
