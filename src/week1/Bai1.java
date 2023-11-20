9b05fd6fca7ab5dc70ff9ad81c91a34ae031b685
chuvietanh392003 <130541979+chuvietanh392003@users.noreply.github.com>
2023-11-20 16:53:32 +0700

    update week 4

--- src/week1/Bai1.java
+++
@@ -1,9 +0,0 @@
-package week1;
-//ChuVietAnh20214983
-
-public class Bai1{
-	public static void main(String[] args) {
-		System.out.println("Xin chao \n cac ban!");
-		System.out.println("Hello \t world!");
-	} //end of method main
-}
